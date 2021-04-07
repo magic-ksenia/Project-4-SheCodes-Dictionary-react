5cbbc1229e69c157467f6f0b80db706a796a3543
magic-ksenia <75555718+magic-ksenia@users.noreply.github.com>
2021-04-07 04:42:30 +0200

    [REMOVE] mapping through synonyms in Definitions.js

--- src/Definitions.js
+++ src/Definitions.js
@@ -3,20 +3,12 @@ import React from "react";
 export default function Definitions(props) {
   return (
     <div className="Definitions">
-      <p>
-        Definition: {props.definition.definition}
-        <br />
-        <em>Example: {props.definition.example}</em>
-        <br />
-        Synonyms:{" "}
-        {props.definition.synonyms.map(function (synonym, index) {
-          if (index < 6) {
-            return <span key={index}>{synonym}, </span>;
-          } else {
-            return null;
-          }
-        })}
-      </p>
+      <p>Definition: {props.definition.definition}</p>
+      <em>Example: {props.definition.example}</em>
+      <p>Synonyms: </p>
     </div>
   );
 }
+/* {props.definition.synonyms.map(function (synonym, index) {
+        return <span key={index}> {synonym}, </span>;
+      })}*/
